d7a3923f9aadde837ed07aeb92e9188ca82eceeb
Sergey Bykov <79042493601@yandex.ru>
2019-05-05 09:14:07 +0400

    Added whoops handler

--- config/autoload/app.global.php
+++ config/autoload/app.global.php
@@ -1,12 +1,15 @@
 <?php
 
 use App\Http\Middleware;
-use App\Http\Middleware\ErrorHandlerMiddleware;
+use Infrastructure\Framework\Http\Middleware\ErrorHandler\PrettyErrorResponseGenerator;
 use Framework\Http\Application;
+use Framework\Http\Middleware\ErrorHandler\ErrorHandlerMiddleware;
+use Framework\Http\Middleware\ErrorHandler\WhoopsErrorResponseGenerator;
 use Framework\Http\Pipeline\MiddlewareResolver;
 use Framework\Http\Router\AuraRouterAdapter;
 use Framework\Http\Router\Router;
 use Framework\Template\TemplateRenderer;
+use Framework\Http\Middleware\ErrorHandler\ErrorResponseGenerator;
 use Psr\Container\ContainerInterface;
 
 return [
@@ -30,9 +33,33 @@ return [
             },
             ErrorHandlerMiddleware::class => function (ContainerInterface $container) {
                 return new ErrorHandlerMiddleware(
-                    $container->get('config')['debug'],
-                    $container->get(TemplateRenderer::class)
+                    $container->get(ErrorResponseGenerator::class)
                 );
+            },
+            ErrorResponseGenerator::class => function (ContainerInterface $container) {
+                if ($container->get('config')['debug']) {
+                    return new WhoopsErrorResponseGenerator(
+                        $container->get(Whoops\RunInterface::class),
+                        new Zend\Diactoros\Response()
+                    );
+                }
+                return new PrettyErrorResponseGenerator(
+                    $container->get(TemplateRenderer::class),
+                    new Zend\Diactoros\Response(),
+                    [
+                        '403' => 'error/403',
+                        '404' => 'error/404',
+                        'error' => 'error/error',
+                    ]
+                );
+            },
+            Whoops\RunInterface::class => function () {
+                $whoops = new Whoops\Run();
+                $whoops->writeToOutput(false);
+                $whoops->allowQuit(false);
+                $whoops->pushHandler(new Whoops\Handler\PrettyPageHandler());
+                $whoops->register();
+                return $whoops;
             },
         ],
     ],
--- config/routes.php
+++ config/routes.php
@@ -4,7 +4,7 @@ use App\Http\Action;
 
 /** @var \Framework\Http\Application $app */
 
-$app->get('home', '/', Action\HelloAction::class);
+$app->get('home', '/', Action\HelloAction::class, ['tokens' => ['id' => '\d+']]);
 $app->get('about', '/about', Action\AboutAction::class);
 //$app->get('cabinet', '/cabinet', Action\CabinetAction::class);
 //$app->get('blog', '/blog', Action\Blog\IndexAction::class);
