48d27906f8fd690cf9ed1dd32ccf69f42c056682
Sergey Bykov <79042493601@yandex.ru>
2019-05-19 20:00:18 +0400

    Added doctrine, migrations, validator

--- config/autoload/console.global.php
+++ config/autoload/console.global.php
@@ -6,13 +6,24 @@ return [
     'dependencies' => [
         'factories' => [
             Command\CacheClearCommand::class => Infrastructure\App\Console\Command\CacheClearCommandFactory::class,
+            Doctrine\Migrations\Tools\Console\Command\DiffCommand::class => Infrastructure\App\Doctrine\Factory\DiffCommandFactory::class,
         ],
     ],
+
     'console' => [
         'commands' => [
             Command\CacheClearCommand::class,
+            Doctrine\Migrations\Tools\Console\Command\ExecuteCommand::class,
+            Doctrine\Migrations\Tools\Console\Command\GenerateCommand::class,
+            Doctrine\Migrations\Tools\Console\Command\LatestCommand::class,
+            Doctrine\Migrations\Tools\Console\Command\MigrateCommand::class,
+            Doctrine\Migrations\Tools\Console\Command\DiffCommand::class,
+            Doctrine\Migrations\Tools\Console\Command\UpToDateCommand::class,
+            Doctrine\Migrations\Tools\Console\Command\StatusCommand::class,
+            Doctrine\Migrations\Tools\Console\Command\VersionCommand::class,
         ],
         'cachePaths' => [
+            'doctrine' => 'var/cache/doctrine',
             'twig' => 'var/cache/twig',
         ],
     ],
---
+++ config/autoload/doctrine.global.php
@@ -0,0 +1,45 @@
+<?php
+
+return [
+    'dependencies' => [
+        'factories'  => [
+            Doctrine\ORM\EntityManagerInterface::class => ContainerInteropDoctrine\EntityManagerFactory::class,
+        ],
+    ],
+
+    'doctrine' => [
+        'configuration' => [
+            'orm_default' => [
+                'result_cache' => 'filesystem',
+                'metadata_cache' => 'filesystem',
+                'query_cache' => 'filesystem',
+                'hydration_cache' => 'filesystem',
+            ],
+        ],
+        'connection' => [
+            'orm_default' => [
+                'driver_class' => \Doctrine\DBAL\Driver\PDOSqlite\Driver::class,
+                'pdo' => PDO::class,
+            ],
+        ],
+        'driver' => [
+            'orm_default' => [
+                'class' => Doctrine\Common\Persistence\Mapping\Driver\MappingDriverChain::class,
+                'drivers' => [
+                    'App\Entity' => 'entities',
+                ],
+            ],
+            'entities' => [
+                'class' => Doctrine\ORM\Mapping\Driver\AnnotationDriver::class,
+                'cache' => 'filesystem',
+                'paths' => ['src/App/Entity'],
+            ],
+        ],
+        'cache' => [
+            'filesystem' => [
+                'class' => Doctrine\Common\Cache\FilesystemCache::class,
+                'directory' => 'var/cache/doctrine',
+            ],
+        ],
+    ],
+];
